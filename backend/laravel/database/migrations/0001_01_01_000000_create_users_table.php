cad2320ce6a66e263a2eca47048a933244480b8d
LeonVonDeusch <dandyzikria@gmail.com>
2026-05-12 02:09:28 +0700

    test docker
    ini aku pake cmd nya wsl jadi mending download dulu wsl ters install docker pake command yang ada di https://docs.docker.com/engine/install/ubuntu/

--- backend/laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -9,41 +9,41 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    // public function up(): void
-    // {
-    //     Schema::create('users', function (Blueprint $table) {
-    //         $table->id();
-    //         $table->varchar('name', 255);
-    //         $table->varchar('email', 255)->unique();
-    //         $table->string('password', 255);
-    //         $table->timestamp('email_verified_at')->nullable();
-    //         $table->timestamps('created_at')->useCurrent();
-    //         $table->timestamps('updated_at')->useCurrentOnUpdate();
-    //     });
+    public function up(): void
+    {
+        // Schema::create('users', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->varchar('name', 255);
+        //     $table->varchar('email', 255)->unique();
+        //     $table->string('password', 255);
+        //     $table->timestamp('email_verified_at')->nullable();
+        //     $table->timestamps('created_at')->useCurrent();
+        //     $table->timestamps('updated_at')->useCurrentOnUpdate();
+        // });
 
-    //     Schema::create('password_reset_tokens', function (Blueprint $table) {
-    //         $table->string('email')->primary();
-    //         $table->string('token');
-    //         $table->timestamp('created_at')->nullable();
-    //     });
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
 
-    //     Schema::create('sessions', function (Blueprint $table) {
-    //         $table->string('id')->primary();
-    //         $table->foreignId('user_id')->nullable()->index();
-    //         $table->string('ip_address', 45)->nullable();
-    //         $table->text('user_agent')->nullable();
-    //         $table->longText('payload');
-    //         $table->integer('last_activity')->index();
-    //     });
-    // }
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
 
-    // /**
-    //  * Reverse the migrations.
-    //  */
-    // public function down(): void
-    // {
-    //     Schema::dropIfExists('users');
-    //     Schema::dropIfExists('password_reset_tokens');
-    //     Schema::dropIfExists('sessions');
-    // }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
 };
